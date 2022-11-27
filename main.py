725c96d8dc7ee2023591bb17f1553776bb0c0e60
Oscar Koeroo <okoeroo@gmail.com>
2022-11-27 14:13:57 +0100

    Cleaned up main.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-from multiprocessing import Pool
 import validators
 
 from library.cli import sanity_checks, argparsing
@@ -12,11 +11,7 @@ from library.csvtools import processor_convert_list_of_fqdnipwhois2csv
 
 ### Main
 if __name__ == '__main__':
-    filename = "pkioverheid.txt"
-    filename = "pkishort.txt"
-    outputfilename = "expanded-output.csv"
-
-    # Parse arguments
+    # Parse arguments and quit if sanity checks fail
     args = argparsing()
     if not sanity_checks(args):
         sys.exit(1)
@@ -25,13 +20,12 @@ if __name__ == '__main__':
     with open(args.input_filename, "r") as f:
         fqdns = [FqdnIpWhois(fqdn.lower()) for fqdn in f.read().splitlines() if validators.domain(fqdn)]
 
-
-#    with Pool(processes=256) as p:
-#        fqdns_with_dns = p.map_async(processor_fqdn2ip, fqdns).get()
+    # DNS query the list, and add to the object list
     fqdns_with_dns = [processor_fqdn2ip(fqdn) for fqdn in fqdns]
 
-
+    # Probe for certificates and expand the object list
     fqdns_with_cert = [processor_probe_certificates(fqdn) for fqdn in fqdns_with_dns]
 
+    # Write output to CSV file
     processor_convert_list_of_fqdnipwhois2csv(args.output_filename, fqdns_with_cert)
 
