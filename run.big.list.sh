be1682687b2bbc7e0be32970173c4e9c2f5e8522
Oscar Koeroo <okoeroo@gmail.com>
2022-11-30 17:45:34 +0100

    Add headers

--- run.big.list.sh
+++ run.big.list.sh
@@ -10,7 +10,10 @@ if [ "$?" != 0 ]; then
 fi
 
 
-cat samples/pkioverheid.expanded-output.csv | grep -i "overheid" | grep -vi "private" > samples/pkioverheid.IN_scope.expanded-output.csv
-cat samples/pkioverheid.expanded-output.csv | grep -v -i "overheid" | grep -vi "private" > samples/pkioverheid.OUT_of_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | head -n 1 > samples/pkioverheid.IN_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | grep -i "overheid" | grep -vi "private" >> samples/pkioverheid.IN_scope.expanded-output.csv
+
+cat samples/pkioverheid.expanded-output.csv | head -n 1 > samples/pkioverheid.OUT_of_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | grep -v -i "overheid" | grep -vi "private" >> samples/pkioverheid.OUT_of_scope.expanded-output.csv
 
 echo "done"
