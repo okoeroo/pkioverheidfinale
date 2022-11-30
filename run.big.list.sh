8d2fd6f668e65005e0977edec39655be19e731bc
Oscar Koeroo <okoeroo@gmail.com>
2022-11-30 17:20:56 +0100

    Filtered out Private Root

--- run.big.list.sh
+++ run.big.list.sh
@@ -10,7 +10,7 @@ if [ "$?" != 0 ]; then
 fi
 
 
-cat samples/pkioverheid.expanded-output.csv | grep -i "overheid" > samples/pkioverheid.IN_scope.expanded-output.csv
-cat samples/pkioverheid.expanded-output.csv | grep -v -i "overheid" > samples/pkioverheid.OUT_of_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | grep -i "overheid" | grep -vi "private" > samples/pkioverheid.IN_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | grep -v -i "overheid" | grep -vi "private" > samples/pkioverheid.OUT_of_scope.expanded-output.csv
 
 echo "done"
