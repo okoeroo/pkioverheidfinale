b06470162503835d25e4d09c7a63ef7dc0555a40
Oscar Koeroo <okoeroo@gmail.com>
2022-12-02 11:30:50 +0100

    fixed in/out of scope selector.

--- run.big.list.sh
+++ run.big.list.sh
@@ -11,9 +11,9 @@ fi
 
 
 cat samples/pkioverheid.expanded-output.csv | head -n 1 > samples/pkioverheid.IN_scope.expanded-output.csv
-cat samples/pkioverheid.expanded-output.csv | grep -i "overheid" | grep -vi "private" >> samples/pkioverheid.IN_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | grep -i "pkioverheid" | grep -vi "private" >> samples/pkioverheid.IN_scope.expanded-output.csv
 
 cat samples/pkioverheid.expanded-output.csv | head -n 1 > samples/pkioverheid.OUT_of_scope.expanded-output.csv
-cat samples/pkioverheid.expanded-output.csv | grep -v -i "overheid" | grep -vi "private" >> samples/pkioverheid.OUT_of_scope.expanded-output.csv
+cat samples/pkioverheid.expanded-output.csv | grep -v -i "pkioverheid" | grep -vi "private" >> samples/pkioverheid.OUT_of_scope.expanded-output.csv
 
 echo "done"
