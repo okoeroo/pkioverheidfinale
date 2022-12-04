f58957d484fa2d1aca13d3b988068bfff80fd693
Oscar Koeroo <okoeroo@gmail.com>
2022-12-04 19:02:43 +0100

    Ugly hacks reformatting

--- library/csvtools.py
+++ library/csvtools.py
@@ -3,8 +3,6 @@ import csv
 import operator
 from library.models import FqdnIpWhois
 
-HACKCODE = 'zzzlazyhackzzz'
-
 
 def generate_csv_row_dict(f: FqdnIpWhois) -> dict:
     row = {}
@@ -16,17 +14,19 @@ def generate_csv_row_dict(f: FqdnIpWhois) -> dict:
 
     
     row['fqdn'] = f.fqdn
-    if f.ip is None:
-        row['ip'] = 'FAILURE'
-    else:
-        row['ip'] = f.ip
-        # row['prefix'] = f.asn.prefix
-        row['asn'] = f.asn.asn
-        row['asn_description'] = f.asn.asn_description.description 
-        row['country'] = f.asn.country 
-        row['registrar'] = f.asn.registrar 
-        row['last_update_asn'] = f.asn.last_update 
-        row['last_update_asn_desc'] = f.asn.asn_description.last_update
+
+#    if f.ip is None:
+#        row['ip'] = 'FAILURE'
+#    else:
+
+    # row['ip'] = f.ip
+    # row['prefix'] = f.asn.prefix
+    row['asn'] = f.asn.asn
+    row['asn_description'] = f.asn.asn_description.description 
+    row['country'] = f.asn.country 
+    row['registrar'] = f.asn.registrar 
+    row['last_update_asn'] = f.asn.last_update 
+    row['last_update_asn_desc'] = f.asn.asn_description.last_update
     
 
     if f.cert is not None:
@@ -36,8 +36,8 @@ def generate_csv_row_dict(f: FqdnIpWhois) -> dict:
         row['not_valid_before'] = f.cert.not_valid_before
         row['not_valid_after'] = f.cert.not_valid_after
         row['common_names'] = f.cert.common_names
-        row['san_dns_names'] = f.cert.san_dns_names
-#        row['san_dns_names'] = f.cert.san_dns_names.replace(",", ",\n")
+#        row['san_dns_names'] = f.cert.san_dns_names
+        row['san_dns_names'] = f.cert.san_dns_names.replace(",", ",\n")
 
         result = re.search('O=([\w\-_ \.\(\)&,\\\\]+),[a-zA-Z]+=', f.cert.subject_dn)
         if result is not None:
@@ -47,15 +47,16 @@ def generate_csv_row_dict(f: FqdnIpWhois) -> dict:
 
 def processor_convert_list_of_fqdnipwhois2csv(outputfilename: str, fqdns_with_dns: list[FqdnIpWhois]) -> None:
     with open(outputfilename, 'w') as csvfile:
-        fieldnames = ['organisation', 'fqdn', 'ip', 'san_dns_names', 
+        fieldnames = ['organisation', 'fqdn',
                         'is_expired',
                         'not_valid_before', 'not_valid_after',
                         # 'prefix',
-                        'asn', 'asn_description', 
-                        'country', 'registrar',
-                        'last_update_asn', 'last_update_asn_desc',
+                        'san_dns_names', 
                         'subject_dn', 'issuer_dn',
-                        'common_names']
+                        'common_names',
+                        'asn', 'asn_description',
+                        'last_update_asn', 'last_update_asn_desc',
+                        'country', 'registrar']
     
         csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         csvwriter.writeheader()
